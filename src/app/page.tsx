4903e5423a5287fd87bc044e8eb08c6ed54f1339
Miguel <thelastcodee@gmail.com>
2026-01-28 21:48:55 -0300

    primeira parte ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import IntroSection from "@/components/sections/IntroSection";
 import GalleryComparison from "@/components/sections/GalleryComparison";
+import Hero from "@/components/sections/Hero";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
-
+      <Hero />
       <IntroSection />
       <GalleryComparison />
     </main>
