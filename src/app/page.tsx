3424d34730287c1b6870c1044dcf93717087eed4
Miguel <thelastcodee@gmail.com>
2026-01-28 22:47:51 -0300

    create about && FAQ sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import IntroSection from "@/components/sections/IntroSection";
 import GalleryComparison from "@/components/sections/GalleryComparison";
 import Hero from "@/components/sections/Hero";
 import FeedbackSection from "@/components/sections/FeedbackSection";
+import FaqSection from "@/components/sections/FaqSection";
+import AboutSection from "@/components/sections/AboutSection";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -11,6 +13,8 @@ export default function Home() {
       <IntroSection />
       <GalleryComparison />
       <FeedbackSection />
+      <AboutSection />
+      <FaqSection />
     </main>
   );
 }
